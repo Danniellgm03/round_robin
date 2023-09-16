14c602fe7568d7873b8c6bf3fd1aa7148d65d627
daniell.gm03 <daniell.gm03@gmail.com>
2023-09-16 16:44:01 +0200

    feat(main): Creamos el main para utilizar la cola

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,27 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Cola<Proceso> cola = new Cola<Proceso>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el numero de procesos");
+        int numProcesos = scanner.nextInt();
+        for(int i = 0; i < numProcesos; i++){
+            cola.addToCola(new Proceso());
+        }
 
+        try {
+            Thread.sleep(2000);
+        }catch (InterruptedException e){}
+
+        System.out.println(cola.toString());
+        while (!cola.isEmpty()){
+            cola.sort();
+            Proceso proceso = cola.sacar();
+            System.out.println("Proceso " + proceso.toString());
+        }
     }
 }
