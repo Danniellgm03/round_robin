9c03faf3aee45e4c8fea1060019e6e5e5260e63c
daniell.gm03 <daniell.gm03@gmail.com>
2023-09-17 18:12:33 +0200

    refactor(main): Quitamos pruebas main
    Quitamos sleep, quitamos que pregunte cuantos procesos se precisa y ponemos 50 fijos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,18 @@
 package org.example;
 
-import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        final int NUM_PROCESOS = 50;
         Cola<Proceso> cola = new Cola<Proceso>();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Introduce el numero de procesos");
-        int numProcesos = scanner.nextInt();
-        for(int i = 0; i < numProcesos; i++){
+
+        for(int i = 0; i < NUM_PROCESOS; i++){
             cola.addToCola(new Proceso());
         }
 
-        try {
-            Thread.sleep(2000);
-        }catch (InterruptedException e){}
-
-        System.out.println(cola.toString());
         while (!cola.isEmpty()){
-            cola.sort();
-            Proceso proceso = cola.sacar();
+            Proceso proceso = cola.extract();
             System.out.println("Proceso " + proceso.toString());
         }
     }
